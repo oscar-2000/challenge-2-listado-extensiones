66f8f8ec28519794f32ecfef30db16adf2595472
oscar-2000 <oscareduvalenzuela@gmail.com>
2025-07-17 22:46:36 -0400

    Change rute images

--- src/assets/components/Search.jsx
+++ src/assets/components/Search.jsx
@@ -27,9 +27,9 @@ export default function Search() {
     <>
         <div className="py-3 px-4 rounded-2xl relative bg-white dark:bg-white/20">
             <div className="flex flex-row gap-2 justify-between items-center">
-                <img src="./public/img/logo.svg" className="h-10 w-auto" alt="Logo" />
+                <img src="/img/logo.svg" className="h-10 w-auto" alt="Logo" />
                 <a href="#" onClick={() => toggleTheme()} className="p-3 rounded-2xl group duration-300 bg-gray-700/20 dark:bg-white/40 dark:hover:bg-white/50">
-                    <img src={`./public/img/icon-${isDark ? 'sun' : 'moon'}.svg`} className="h-6 opacity-80 group-hover:opacity-100 duration-300" alt={`Modo ${isDark ? 'oscuro' : 'claro'}`} />
+                    <img src={`/img/icon-${isDark ? 'sun' : 'moon'}.svg`} className="h-6 opacity-80 group-hover:opacity-100 duration-300" alt={`Modo ${isDark ? 'oscuro' : 'claro'}`} />
                 </a>
             </div>
         </div>
